887eb8b5003265e4e07045d88a2086b276a4c5ea
DennisPie <101339698+DennisPie@users.noreply.github.com>
2022-03-10 09:35:05 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -6,10 +6,12 @@ input.onGesture(Gesture.TiltLeft, function () {
 })
 input.onButtonPressed(Button.B, function () {
     basic.showString("Good Job")
-    music.playMelody("F F F C5 C5 C5 C5 C5 ", 500)
-    music.playMelody("C5 G G G E G C5 C5 ", 500)
-    music.playMelody("A G G B C5 C5 C5 C5 ", 500)
-    music.playMelody("F A B F D - A C5 ", 500)
+    music.playMelody("C5 C5 C5 B B F F F ", 120)
+    music.playMelody("C5 C5 B E E E E E ", 120)
+    music.playMelody("F F F C5 C5 C5 C5 C5 ", 120)
+    music.playMelody("C5 G G G E G C5 C5 ", 120)
+    music.playMelody("A G G B C5 C5 C5 C5 ", 120)
+    music.playMelody("F A C5 G D C C C ", 120)
     basic.clearScreen()
 })
 input.onGesture(Gesture.Shake, function () {
